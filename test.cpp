831c9bab4b69c10e9cc0bf78dc5b1969709f0820
Cadaing, Sebastian Mhel Tagacay <sebastiancadaing1@gmail.com>
2025-01-23 08:04:55 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -8,18 +8,19 @@ using namespace std;
 
 mutex traffic_mutex;
 condition_variable cv;
-bool greenForAandC = true;
+bool greenForGroupAC = true; 
 
-void trafficLight(const string& roadName, bool isGroupA, int greenDuration, int yellowDuration, int redDuration) {
+void trafficLight(const string& roadName, bool isGroupAC, int greenDuration, int yellowDuration, int redDuration) {
     while (true) {
         {
             unique_lock<mutex> lock(traffic_mutex);
-            cv.wait(lock, [isGroupA] { return greenForAandC == isGroupA; });
+            cv.wait(lock, [isGroupAC] { return greenForGroupAC == isGroupAC; });
 
             cout << roadName << ": GREEN light (Go!)" << endl;
+            cout << roadName << ": LEFT-TURN SIGNAL (Turn left if safe!)" << endl;
             lock.unlock();
             this_thread::sleep_for(chrono::seconds(greenDuration));
-            
+
             lock.lock();
             cout << roadName << ": YELLOW light (Prepare to stop)" << endl;
             lock.unlock();
@@ -29,10 +30,11 @@ void trafficLight(const string& roadName, bool isGroupA, int greenDuration, int 
             cout << roadName << ": RED light (Stop!)" << endl;
         }
 
-        if ((isGroupA && roadName == "Road C") || (!isGroupA && roadName == "Road D")) {
+        
+        if ((isGroupAC && roadName == "Road C") || (!isGroupAC && roadName == "Road D")) {
             {
                 lock_guard<mutex> lock(traffic_mutex);
-                greenForAandC = !greenForAandC;
+                greenForGroupAC = !greenForGroupAC;
             }
             cv.notify_all();
         }
@@ -42,15 +44,35 @@ void trafficLight(const string& roadName, bool isGroupA, int greenDuration, int 
 }
 
 int main() {
-    const int greenDuration = 5;
+    const int greenDuration = 5; 
     const int yellowDuration = 2;
-    const int redDuration = 7;
+    const int redDuration = 7;  
+    char initialRoad; 
 
+    
+    cout << "Enter the road to start with green light (A, B, C, D): ";
+    cin >> initialRoad;
+
+ 
+    while (initialRoad != 'A' && initialRoad != 'B' && initialRoad != 'C' && initialRoad != 'D') {
+        cout << "Invalid input. Please enter A, B, C, or D: ";
+        cin >> initialRoad;
+    }
+
+ 
+    if (initialRoad == 'A' || initialRoad == 'C') {
+        greenForGroupAC = true;
+    } else {
+        greenForGroupAC = false;
+    }
+
+ 
     thread roadA(trafficLight, "Road A", true, greenDuration, yellowDuration, redDuration);
     thread roadB(trafficLight, "Road B", false, greenDuration, yellowDuration, redDuration);
     thread roadC(trafficLight, "Road C", true, greenDuration, yellowDuration, redDuration);
     thread roadD(trafficLight, "Road D", false, greenDuration, yellowDuration, redDuration);
 
+   
     roadA.join();
     roadB.join();
     roadC.join();
